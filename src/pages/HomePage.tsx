93a648c92c438af3423345aca0842aa7fa7605b0
hugopuybareau <hugo.puybareau@gmail.com>
2025-09-09 15:46:40 +0200

    feat: added X link + deleted the signature legacy code

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -23,6 +23,18 @@ const fadeIn = {
     }),
 };
 
+const XIcon = ({ size = 20 }: { size?: number }) => (
+    <svg
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+    </svg>
+);
+
 const HomePage: React.FC = () => {
     return (
         <motion.div
@@ -239,6 +251,10 @@ const HomePage: React.FC = () => {
                         {
                             icon: <Linkedin size={20} />,
                             href: "https://www.linkedin.com/in/hugopuybareau/",
+                        },
+                        {
+                            icon: <XIcon size={20} />,
+                            href: "https://x.com/hugopuybareau",
                         },
                         {
                             icon: <Mail size={20} />,
