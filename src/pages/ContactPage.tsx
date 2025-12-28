9c651e9260b5ff39d167c8f133ff3f1c7a1efeeb
hugopuybareau <hugo.puybareau@gmail.com>
2025-12-28 14:46:34 +0100

    feat(space): old commit idk

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { Mail, MapPin, Phone, Github, Linkedin, Code } from "lucide-react";
+import { Mail, MapPin, Phone, Github, Linkedin, Code} from "lucide-react";
 
 
 const fadeIn = {
